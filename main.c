4d24175eb0cf44a2e6c0b2fdee70f548ae6dca40
H.K <16552133+bj1024@users.noreply.github.com>
2021-09-14 19:15:32 +0900

    Add logcat output.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <time.h>
 
+#include <android/log.h>	// Android Logging
+
 double monotnic_time()
 {
 	struct timespec ts;
@@ -17,6 +19,10 @@ int main(int argc, char **argv)
 	double curtime = monotnic_time();
 
 	printf("%.6f Android NDK sample program.\n",curtime);
+
+	__android_log_print(ANDROID_LOG_INFO,
+				    "AndroidNDKSample",
+					"%.6f Android NDK sample program",curtime);
 
 	return 0;
 }
